c6c3d25ba0ee76af5f57749de42b5c61c79d0a4e
kristianbraathen <97292975+kristianbraathen@users.noreply.github.com>
2023-03-30 14:05:35 +0200

    Add files via upload

---
+++ Data/AppilcationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Jokes.Models;
+
+namespace Jokes.Data
+{
+    public class AppilcationDbContext : DbContext
+    {
+        public AppilcationDbContext (DbContextOptions<AppilcationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Jokes.Models.Joke> Joke { get; set; }
+    }
+}
